7194e9b4c5156e67c8e1d6f09f2f38d8ce56cd1f
warclib <36675461+warclib@users.noreply.github.com>
2026-02-26 22:11:52 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'otimac-cache-v1';
+const urlsToCache = ['./', './index.html', './manifest.json', './icono-otimac.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
