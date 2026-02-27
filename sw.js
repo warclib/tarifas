e6a382695a4e592f169735f1ebb6f8b23ef4662a
warclib <36675461+warclib@users.noreply.github.com>
2026-02-27 11:14:57 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,10 @@
-self.addEventListener('fetch', () => {});
+const CACHE_NAME = 'otimac-offline-v3';
+const assets = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
