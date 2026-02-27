ff3b5779596771e15517098e8ebea3045b3aac11
warclib <36675461+warclib@users.noreply.github.com>
2026-02-26 22:31:58 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,1 @@
-const CACHE_NAME = 'otimac-v1';
-const assets = ['./', './index.html', './manifest.json', './icono-otimac.png'];
-
-self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(assets)));
-});
-
-self.addEventListener('fetch', (e) => {
-    e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
-});
+self.addEventListener('fetch', () => {});
